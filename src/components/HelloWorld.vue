45013cb68c3ac2686d298306f6271c76718a1ab8
Nipun Bathini <80241460+Nipun79@users.noreply.github.com>
2021-10-12 11:38:35 +0530

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,36 @@
 <template>
   <v-container>
-      {{data.msg}}
+      <form>
+    <v-text-field
+      v-model="name"
+      :error-messages="nameErrors"
+      :counter="10"
+      label="Name"
+      required
+      @input="$v.name.$touch()"
+      @blur="$v.name.$touch()"
+    ></v-text-field>
+    <v-text-field
+      v-model="password"
+      :error-messages="emailErrors"
+      label="Password"
+      type="password"
+      required
+      @input="$v.email.$touch()"
+      @blur="$v.email.$touch()"
+    ></v-text-field>
+    
+    
+
+    <v-btn
+      class="mr-4"
+      @click="submit"
+    >
+      submit
+    </v-btn>
+    
+  </form>
+  {{tasks}}
   </v-container>
 </template>
 
@@ -8,8 +38,43 @@
   import Vue from 'vue'
   import Component from 'vue-class-component'
   import {vxm} from "../store/store.vuex"
-  @Component()
+  @Component({
+    
+  })
   export default class extends Vue{
-data=vxm.utility
+
+    name=""
+    password=""
+    tasks=[]
+  
+    submit (){
+      //   const res = fetch(`http://localhost:5000/users`)
+      //   const data =  res.json()
+      //   this.users=data
+      //   this.users.forEach((item)=>{
+      //     if(item.name===this.name && this.password===item.password){
+      //       alert("exists")
+      //     }
+      //     else{
+      //       alert("not exists")
+      //     }
+      //   })
+      // },
+     fetch('http://localhost:5000/users')
+     .then(res=>res.json())
+     .then((item)=>{
+       if(item.some(data => data.name===this.name && data.password==this.password)){
+         alert('exists')
+         this.$router.push(this.$route.query.redirect || '/about')
+       }
+       else{
+         alert('does not exist')
+       }
+     })
+     
+      
+      }
+
+     
   }
 </script>
